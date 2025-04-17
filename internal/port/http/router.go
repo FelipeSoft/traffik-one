96dffcd0c026792ace4fb34dd122534be39f0eb7
FelipeSoft <felipedecastrolima2@gmail.com>
2025-04-17 18:20:44 -0300

    chore: including legibility for the current code inside routes file

--- internal/port/http/router.go
+++ internal/port/http/router.go
@@ -8,14 +8,24 @@ import (
 func RegisterRoutes(app *app.App) *port.Router {
 	router := port.NewRouter()
 
-	router.Handle("POST", "/test", app.Middlewares.AuthenticationMiddleware.HandleBearerToken(app.Handlers.TestHandler.Test()))
-	router.Handle("POST", "/backends/add", app.Middlewares.AuthenticationMiddleware.HandleBearerToken(app.Handlers.BackendHandler.AddBackend()))
-	router.Handle("GET", "/backends", app.Middlewares.AuthenticationMiddleware.HandleBearerToken(app.Handlers.BackendHandler.GetAllBackends()))
-	router.Handle("PUT", "/backends/{backendId}/update", app.Middlewares.AuthenticationMiddleware.HandleBearerToken(app.Handlers.BackendHandler.UpdateBackend()))
-	router.Handle("PATCH", "/backends/{backendId}/activate", app.Middlewares.AuthenticationMiddleware.HandleBearerToken(app.Handlers.BackendHandler.ActivateBackend()))
-	router.Handle("PATCH", "/backends/{backendId}/inactivate", app.Middlewares.AuthenticationMiddleware.HandleBearerToken(app.Handlers.BackendHandler.InactivateBackend()))
-	router.Handle("DELETE", "/backends/{backendId}/delete", app.Middlewares.AuthenticationMiddleware.HandleBearerToken(app.Handlers.BackendHandler.DeleteBackend()))
-	router.Handle("GET", "/backends/{backendId}/find", app.Middlewares.AuthenticationMiddleware.HandleBearerToken(app.Handlers.BackendHandler.GetBackendByID()))
+	withBearerToken := app.Middlewares.AuthenticationMiddleware.HandleBearerToken
+
+	router.Handle("POST", "/test",
+		withBearerToken(app.Handlers.TestHandler.Test()))
+	router.Handle("POST", "/backends/add",
+		withBearerToken(app.Handlers.BackendHandler.AddBackend()))
+	router.Handle("GET", "/backends",
+		withBearerToken(app.Handlers.BackendHandler.GetAllBackends()))
+	router.Handle("PUT", "/backends/{backendId}/update",
+		withBearerToken(app.Handlers.BackendHandler.UpdateBackend()))
+	router.Handle("PATCH", "/backends/{backendId}/activate",
+		withBearerToken(app.Handlers.BackendHandler.ActivateBackend()))
+	router.Handle("PATCH", "/backends/{backendId}/inactivate",
+		withBearerToken(app.Handlers.BackendHandler.InactivateBackend()))
+	router.Handle("DELETE", "/backends/{backendId}/delete",
+		withBearerToken(app.Handlers.BackendHandler.DeleteBackend()))
+	router.Handle("GET", "/backends/{backendId}/find",
+		withBearerToken(app.Handlers.BackendHandler.GetBackendByID()))
 
 	return router
 }
